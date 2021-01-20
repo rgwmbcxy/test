c54163dc65d872ddb5653e84c20529f5fe094a7d
zy <1273068089@qq.com>
2021-01-20 21:51:01 +0800

    测试

---
+++ src/main/java/com/zy/test/Github.java
@@ -0,0 +1,16 @@
+package com.zy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zy
+ * @createTime 2021/1/20 19:02
+ * @description
+ */
+@SpringBootApplication
+public class Github {
+    public static void main(String[] args) {
+        SpringApplication.run(Github.class,args);
+    }
+}
